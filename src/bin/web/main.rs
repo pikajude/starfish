aae9df82f0e5005afbf3f4a86c9fb7bc8b26166a
Jude Taylor <jude.taylor@dfinity.org>
2023-03-10 17:00:18 -0800

    actix

---
+++ src/bin/web/main.rs
@@ -0,0 +1,116 @@
+#![feature(try_blocks)]
+
+use actix_web::http::header::Accept;
+use actix_web::web::{self, Json};
+use actix_web::{get, guard, App, HttpResponse, HttpServer, Responder};
+use askama::Template;
+use sqlx::PgPool;
+use starfish::{Build, WorkerConfig};
+
+mod tail;
+
+mod config {
+  use serde::Deserialize;
+
+  #[derive(Deserialize, Debug)]
+  pub struct StarfishConfig {
+    pub database_url: String,
+  }
+}
+
+fn wrap<T, E: std::error::Error + 'static>(thing: Result<T, E>) -> actix_web::Result<T> {
+  thing.map_err(|e| actix_web::error::ErrorInternalServerError(e))
+}
+
+fn content_type_guard<E>(ty: E) -> impl guard::Guard
+where
+  mime::Mime: PartialEq<E>,
+{
+  guard::fn_guard(move |ctx| {
+    ctx
+      .header::<Accept>()
+      .map_or(false, |h| h.preference() == ty)
+  })
+}
+
+async fn index() -> impl Responder {
+  #[derive(Template)]
+  #[template(path = "index.html")]
+  struct IndexPage {
+    version: &'static str,
+    sha: &'static str,
+  }
+
+  HttpResponse::Ok().content_type(mime::TEXT_HTML_UTF_8).body(
+    IndexPage {
+      version: env!("VERGEN_GIT_SEMVER"),
+      sha: env!("VERGEN_GIT_SHA"),
+    }
+    .render()
+    .unwrap(),
+  )
+}
+
+#[get("/builds")]
+async fn get_builds(db: web::Data<PgPool>) -> actix_web::Result<impl Responder> {
+  Ok(Json(wrap(
+    sqlx::query_as!(
+      Build,
+      "SELECT id, origin, created_at, error_msg, finished_at, rev, status as \"status: _\" FROM \
+       builds ORDER BY created_at DESC LIMIT 10"
+    )
+    .fetch_all(&**db)
+    .await,
+  )?))
+}
+
+#[get("/build/{id}")]
+async fn get_build(db: web::Data<PgPool>, id: web::Path<i32>) -> actix_web::Result<impl Responder> {
+  let build = match wrap(Build::get(*id, &**db).await)? {
+    Some(b) => b,
+    None => return Ok(None),
+  };
+
+  let inputs = wrap(build.get_inputs_and_outputs(&**db).await)?;
+
+  Ok(Some(Json(
+    serde_json::json!({ "build": build, "inputs": inputs }),
+  )))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+  let cfg_ = ::config::Config::builder()
+    .add_source(::config::Environment::default())
+    .build()?;
+
+  let cfg = cfg_.try_deserialize::<config::StarfishConfig>()?;
+
+  let pg = PgPool::connect(&cfg.database_url).await?;
+
+  Ok(
+    HttpServer::new(move || {
+      App::new()
+        .service(actix_files::Files::new("/static", "dist").show_files_listing())
+        .service(
+          web::scope("/api")
+            .guard(content_type_guard("application/json"))
+            .service(get_builds)
+            .service(get_build),
+        )
+        .service(web::scope("/api").service(tail::get_build_tail))
+        .route(
+          "/{_:.*}",
+          web::get().guard(content_type_guard("text/html")).to(index),
+        )
+        .app_data(web::Data::new(pg.clone()))
+        .app_data(web::Data::new(WorkerConfig::default()))
+        .wrap(actix_web::middleware::Logger::default())
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await?,
+  )
+}
---
+++ src/bin/web/tail.rs
@@ -0,0 +1,155 @@
+use std::borrow::Cow;
+use std::fs::File;
+use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::path::Path;
+
+use actix_web::{get, web, Responder};
+use actix_web_lab::sse;
+use futures_util::StreamExt;
+use inotify::{EventMask, Inotify, WatchMask};
+use serde::{Deserialize, Serialize};
+use starfish::{BoxDynError, WorkerConfig};
+
+#[derive(Deserialize)]
+pub struct LenSpec {
+  len: Option<usize>,
+}
+
+#[get("/build/{id}/tail")]
+pub async fn get_build_tail(
+  wc: web::Data<WorkerConfig>,
+  id: web::Path<i32>,
+  len: web::Query<LenSpec>,
+) -> actix_web::Result<impl Responder> {
+  let tail_len = len.len.unwrap_or(20);
+  let log_path = wc.logfile(*id);
+
+  let logfile = File::open(&log_path)?;
+
+  let (sender, sse_stream) = actix_web_lab::sse::channel(10);
+
+  let jh = actix_web::rt::spawn(async move {
+    if let Err(e) = tail_the_file(sender, &log_path, logfile, tail_len).await {
+      log::info!("client thread exited: {:?}", e);
+    }
+  });
+  std::mem::forget(jh);
+
+  Ok(sse_stream.with_retry_duration(std::time::Duration::from_secs(10)))
+}
+
+async fn tail_the_file(
+  sender: sse::Sender,
+  log_path: &Path,
+  mut logfile: File,
+  tail_len: usize,
+) -> Result<(), Box<dyn std::error::Error>> {
+  let tailhead = tailme(&mut logfile, tail_len, b'\n')?;
+
+  #[derive(Serialize)]
+  #[serde(tag = "t", content = "c")]
+  enum TailEvent<'s> {
+    Text(Cow<'s, str>),
+    Error(String),
+    Reset,
+  }
+
+  macro_rules! yield_ {
+    ($e:expr) => {
+      sender
+        .send(sse::Event::Data(sse::Data::new_json(&$e).unwrap()))
+        .await?;
+    };
+  }
+
+  yield_!(TailEvent::Text(String::from_utf8_lossy(&tailhead)));
+
+  let mut notifier = Inotify::init()?;
+  notifier.add_watch(log_path, WatchMask::CREATE | WatchMask::MODIFY)?;
+  let buffer = vec![0u8; 1024];
+  let mut notifs = notifier.event_stream(buffer)?;
+
+  while let Some(ev) = notifs.next().await {
+    let event_err: Result<(), BoxDynError> = try {
+      let ev = ev?;
+      if ev.mask.contains(EventMask::MODIFY) {
+        let mut v = vec![];
+        logfile.read_to_end(&mut v)?;
+        if !v.is_empty() {
+          yield_!(TailEvent::Text(String::from_utf8_lossy(&v)));
+        }
+      } else if ev.mask.contains(EventMask::CREATE) {
+        yield_!(TailEvent::Reset);
+        break;
+      }
+    };
+    if let Err(e) = event_err {
+      yield_!(TailEvent::Error(e.to_string()));
+      break;
+    }
+  }
+
+  Ok(())
+}
+
+// it may not look like much, but it's honest work (copied from GNU tail)
+fn tailme(fd: &mut File, mut n_lines: usize, sep: u8) -> std::io::Result<Vec<u8>> {
+  let end_pos = fd.seek(SeekFrom::End(0))?;
+  let start_pos = 0;
+
+  const BUFSIZE: usize = 1024;
+  let mut buffer = [0u8; BUFSIZE];
+  let mut pos = end_pos;
+
+  if n_lines == 0 {
+    return Ok(vec![]);
+  }
+
+  let mut bytes_read = ((pos - start_pos) as usize) % BUFSIZE;
+  if bytes_read == 0 {
+    bytes_read = BUFSIZE;
+  }
+
+  pos -= bytes_read as u64;
+  fd.seek(SeekFrom::Start(pos as _))?;
+  bytes_read = fd.read(&mut buffer)?;
+  if bytes_read > 0 && buffer[bytes_read - 1] != sep {
+    n_lines -= 1;
+  }
+
+  loop {
+    let mut n = bytes_read;
+    while n > 0 {
+      n = match buffer[..n].iter().rposition(|c| *c == sep) {
+        Some(c) => c,
+        None => break,
+      };
+      if n_lines == 0 {
+        let mut v = buffer[n + 1..bytes_read].to_vec();
+        BufReader::new(fd)
+          .take(end_pos - (pos + bytes_read as u64))
+          .read_to_end(&mut v)?;
+        return Ok(v);
+      }
+      n_lines -= 1;
+    }
+
+    if pos == start_pos {
+      fd.seek(SeekFrom::Start(start_pos))?;
+      let mut v = vec![];
+      BufReader::new(fd).take(end_pos).read_to_end(&mut v)?;
+      return Ok(v);
+    }
+
+    pos -= BUFSIZE as u64;
+    fd.seek(SeekFrom::Start(pos))?;
+
+    bytes_read = fd.read(&mut buffer)?;
+
+    if bytes_read == 0 {
+      break;
+    }
+  }
+
+  Ok(vec![])
+}
